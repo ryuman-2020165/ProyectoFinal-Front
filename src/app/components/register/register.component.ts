4beeb48a735bf79bc09de80c8020876efb1eb95c
flopez-2020056 <flopez-2020056@kinal.edu.gt>
2022-07-15 23:36:14 -0600

    Creación de register

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from 'src/app/models/user.model';
+import { UserRestService } from 'src/app/services/userRest/user-rest.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,12 +9,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  user:UserModel;
+  repeatPassword: string = '';
+  timer: any;
+  hola: string = '';
+  
 
-  constructor() { }
+  constructor(
+    private userRest: UserRestService,
+    private router: Router
+  ) { 
+    this.user = new UserModel('', '', '', '', '', '', '', '','');
+  }
 
   ngOnInit(): void {
   }
 
+  ///LOS MÉTODOS QUE CREAMOS SIEMPRE VAN DESPUÉS DEL ngOnInit()
 
-  
+  async checkPassword(){
+    clearTimeout(this.timer);
+    this.timer = await setTimeout(()=>{
+      if(this.repeatPassword != this.user.password){
+        alert('Password doesnt match');
+        clearTimeout(this.timer);
+      }else{
+        alert('Password match');
+        clearTimeout(this.timer);
+      }
+    }, 1500)
+  }
+
+
+
+  register(){
+    console.log(this.user);
+    this.userRest.register(this.user).subscribe({
+      next: (response:any)=>{
+        alert(response.message + ' you can login now');
+        this.router.navigateByUrl('/login');
+      },
+      error: (err)=> alert(err.error.message || err.error)
+    })
+  }
+
 }
