8130f1173cc9c7a266d301b4f9aadcd81e227002
ryuman-2020165 <ryuman-2020165@kinal.edu.gt>
2022-07-15 23:08:43 -0600

    Creación de la función login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserRestService } from 'src/app/services/userRest/user-rest.service';
+import { Router } from '@angular/router';
+import { UserModel } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  user: UserModel;
 
-  constructor() { }
+  constructor(
+    private userRest: UserRestService, 
+    private router: Router) 
+    { 
+    this.user = new UserModel('', '', '', '', '', '', '', '','');
+    }
 
   ngOnInit(): void {
   }
 
+
+  login(){
+    this.userRest.login(this.user).subscribe({
+      next: (res: any)=>{
+        console.log(res);
+        localStorage.setItem('token', res.token);
+        localStorage.setItem('identity', JSON.stringify(res.checkUser));
+        this.router.navigateByUrl('/home');
+      },
+      error: (err: any)=>{
+        alert(err.error.message || err.error)
+        
+      }
+
+    })
+  }
 }
